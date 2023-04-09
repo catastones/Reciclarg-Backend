3e82357aa487dd62856c12d3c1b95d71c58fac83
luciareynaga <luciareynaga207@gmail.com>
2023-04-09 12:15:42 -0300

    creacion de entorno de desarrollo con maven, java 11 y dependencias

---
+++ src/main/java/cloud/reciclarg/ReciclargAppApplication.java
@@ -0,0 +1,13 @@
+package cloud.reciclarg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclargAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclargAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/reciclarg/ReciclargAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.reciclarg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclargAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
