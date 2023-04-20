7a389a873021465b90f59a2138f8187cfa944165
Cristian Alderete <catastones@gmail.com>
2023-04-19 21:03:29 -0300

    Repositorio y Service Para usuarios

---
+++ Backend/src/main/java/com/Reciclarg/Backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+
+package com.Reciclarg.Backend.repository;
+
+import com.Reciclarg.Backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository <User, Long>  {
+    public User findByUsuario(String username);
+}
---
+++ Backend/src/main/java/com/Reciclarg/Backend/service/IUserService.java
@@ -0,0 +1,14 @@
+
+package com.Reciclarg.Backend.service;
+
+import com.Reciclarg.Backend.model.User;
+import java.util.List;
+
+
+public interface IUserService {
+    public User buscarUserByName(String username);
+    //ublic List<User> VerUsuarios();
+    public void SaveUsuario(User user);
+    public void borrarUsuario(Long id);
+    public User buscarUserById(Long id);
+}
